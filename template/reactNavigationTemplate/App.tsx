28da84635c0990e4e11d1339f2ff1f4c299afaf8
Ahmed ALAbsi <alabsi91@gmail.com>
2024-01-02 14:30:26 +0300

    Update template: Refactor App and navigation

--- template/reactNavigationTemplate/App.tsx
+++ template/reactNavigationTemplate/App.tsx
@@ -1,31 +1,16 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import { StatusBar } from 'react-native';
-import { enableFreeze } from 'react-native-screens';
 
 import Confirm from '@components/Confirm';
 import Portal from '@components/Portal/Portal';
 import Toast from '@components/ToastMessage';
-import Home from '@screens/Home';
-import { ThemeProvider, useTheme } from '@styles/Theme';
-
-import type { RootStackParamList } from '@types';
-
-enableFreeze(true);
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
+import { ThemeProvider } from '@styles/Theme';
+import MainNavigation from './navigation/MainNavigation';
 
 export default function App() {
   return (
     <ThemeProvider>
-      <ThemedStatusBar />
       <Portal.Host>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName='Home' screenOptions={{ header: () => null }}>
-            <Stack.Screen name='Home' component={Home} />
-          </Stack.Navigator>
-        </NavigationContainer>
+        <MainNavigation />
       </Portal.Host>
 
       {/* Everything included here will be rendered on top of the app.  */}
@@ -33,9 +18,4 @@ export default function App() {
       <Toast.Provider />
     </ThemeProvider>
   );
-}
-
-function ThemedStatusBar() {
-  const theme = useTheme();
-  return <StatusBar backgroundColor='transparent' barStyle={theme.isDark ? 'light-content' : 'dark-content'} translucent />;
 }
---
+++ template/reactNavigationTemplate/navigation/MainNavigation.tsx
@@ -0,0 +1,29 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { enableFreeze } from 'react-native-screens';
+
+import Home from '@screens/Home';
+import { useTheme } from '@styles/Theme';
+
+import type { RootStackParamList } from '@types';
+
+enableFreeze(true);
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function MainNavigation() {
+  const theme = useTheme();
+
+  return (
+    <>
+      <StatusBar backgroundColor='transparent' barStyle={theme.isDark ? 'light-content' : 'dark-content'} translucent />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home' screenOptions={{ header: () => null }}>
+          <Stack.Screen name='Home' component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
